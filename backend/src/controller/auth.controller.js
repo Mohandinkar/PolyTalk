7898d1a77209e1209f899402a2911e26b5b49716
Mohandinkar <mohandinkar75@gmail.com>
2025-11-21 15:08:36 +0530

    signUp endpoint

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,7 +1,56 @@
-import e from "express";
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 
-export function signup(req,res){
-    res.send("Signup API");
+export async function signup(req,res){
+    const {email, password, fullName} = req.body;
+
+    try {
+        
+        if(!email || !password || !fullName){
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters"});
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message: "Invalid email format"});
+        }
+
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "User already exists"});
+        }
+
+        //create the profile randomly 
+        const idx = Math.floor(Math.random() * 100) + 1;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+        const newUser = await User.create({
+            email,
+            password,
+            fullName,
+            profilePic: randomAvatar
+        });
+
+        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET, {
+            expiresIn: '7d'
+        });
+
+        res.cookie('jwt',token, {
+            maxAge: 7*24*60*60*1000,
+            httpOnly: true,
+            sameSite : 'strict',
+            secure: process.env.NODE_ENV === 'production'
+        })
+
+        res.status(201).json({success: true, user:newUser});
+
+    } catch (error) {
+        console.error("Error in signup:", error);
+        res.status(500).json({message: "Server error"});
+    }
 }
 export function login(req,res){
     res.send("Login API");
---
+++ backend/src/models/User.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      min: 2,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    nativeLang: {
+      type: String,
+      default: "",
+    },
+    learningLang: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    isOnboarded: {
+      type: Boolean,
+      default: false,
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre('save', async function(next){
+
+    if(!this.isModified('password')){
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
